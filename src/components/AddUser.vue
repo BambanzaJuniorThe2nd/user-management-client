41ab79bcc86686bd99579cd59cf88484df3be266
Jason Mahirwe <bjthe2nd@gmail.com>
2021-07-07 12:22:20 +0200

    Install vee-validate

--- src/components/AddUser.vue
+++ src/components/AddUser.vue
@@ -2,38 +2,161 @@
   <div class="submit-form mt-3 mx-auto">
     <p class="headline">Add User</p>
 
+    <validation-observer
+      ref="observer"
+      v-slot="{ invalid }"
+    >
+      <form @submit.prevent="submit">
+        <validation-provider
+          v-slot="{ errors }"
+          name="Name"
+          rules="required|max:10"
+        >
+          <v-text-field
+            v-model="name"
+            :counter="10"
+            :error-messages="errors"
+            label="Name"
+            required
+          ></v-text-field>
+        </validation-provider>
+        <validation-provider
+          v-slot="{ errors }"
+          name="phoneNumber"
+          :rules="{
+            required: true,
+            digits: 7,
+            regex: '^(71|72|74|76|81|82|84|85|86|87|88|89)\\d{5}$'
+          }"
+        >
+          <v-text-field
+            v-model="phoneNumber"
+            :counter="7"
+            :error-messages="errors"
+            label="Phone Number"
+            required
+          ></v-text-field>
+        </validation-provider>
+        <validation-provider
+          v-slot="{ errors }"
+          name="email"
+          rules="required|email"
+        >
+          <v-text-field
+            v-model="email"
+            :error-messages="errors"
+            label="E-mail"
+            required
+          ></v-text-field>
+        </validation-provider>
+        <validation-provider
+          v-slot="{ errors }"
+          name="select"
+          rules="required"
+        >
+          <v-select
+            v-model="select"
+            :items="items"
+            :error-messages="errors"
+            label="Select"
+            data-vv-name="select"
+            required
+          ></v-select>
+        </validation-provider>
+        <validation-provider
+          v-slot="{ errors }"
+          rules="required"
+          name="checkbox"
+        >
+          <v-checkbox
+            v-model="checkbox"
+            :error-messages="errors"
+            value="1"
+            label="Option"
+            type="checkbox"
+            required
+          ></v-checkbox>
+        </validation-provider>
+
+        <v-btn
+          class="mr-4"
+          type="submit"
+          :disabled="invalid"
+        >
+          submit
+        </v-btn>
+        <v-btn @click="clear">
+          clear
+        </v-btn>
+      </form>
+    </validation-observer>
+
     <div v-if="!submitted">
       <v-form ref="form" lazy-validation>
         <v-text-field
           v-model="user.name"
-          :rules="[(v) => !!v || 'Name is required']"
+          :rules="nameRules"
           label="Name"
           required
         ></v-text-field>
 
         <v-text-field
           v-model="user.email"
-          :rules="[(v) => !!v || 'Email is required']"
-          label="Email"
+          :rules="emailRules"
+          label="E-mail"
           required
         ></v-text-field>
 
         <v-text-field
           v-model="user.title"
-          :rules="[(v) => !!v || 'Title is required']"
+          :rules="titleRules"
           label="Title"
           required
         ></v-text-field>
 
-        <v-text-field
-          v-model="user.birthdate"
-          :rules="[(v) => !!v || 'Birthdate is required']"
-          label="Birthdate"
-          required
-        ></v-text-field>
+        <v-col cols="12" sm="12">
+          <v-dialog
+            ref="dialog"
+            v-model="modal"
+            :return-value.sync="user.birthdate"
+            persistent
+            width="290px"
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-text-field
+                v-model="user.birthdate"
+                label="Pick a birthdate"
+                prepend-icon="mdi-calendar"
+                readonly
+                v-bind="attrs"
+                v-on="on"
+              ></v-text-field>
+            </template>
+            <v-date-picker
+              v-model="user.birthdate"
+              scrollable
+            >
+              <v-spacer></v-spacer>
+              <v-btn
+                text
+                color="primary"
+                @click="modal = false"
+              >
+                Cancel
+              </v-btn>
+              <v-btn
+                text
+                color="primary"
+                @click="$refs.dialog.save(user.birthdate)"
+              >
+                OK
+              </v-btn>
+            </v-date-picker>
+          </v-dialog>
+        </v-col>
       </v-form>
 
-      <v-btn color="primary" class="mt-3" @click="saveTutorial">Submit</v-btn>
+      <v-btn color="primary" class="mt-3" @click="saveUser">Submit</v-btn>
     </div>
 
     <div v-else>
@@ -55,6 +178,35 @@
 </template>
 
 <script>
+import { required, digits, email, max, regex } from 'vee-validate/dist/rules'
+import { extend, ValidationObserver, ValidationProvider, setInteractionMode } from 'vee-validate'
+
+setInteractionMode('eager')
+
+extend('digits', {
+  ...digits,
+  message: '{_field_} needs to be {length} digits. ({_value_})',
+})
+
+extend('required', {
+  ...required,
+  message: '{_field_} can not be empty',
+})
+
+extend('max', {
+  ...max,
+  message: '{_field_} may not be greater than {length} characters',
+})
+
+extend('regex', {
+  ...regex,
+  message: '{_field_} {_value_} does not match {regex}',
+})
+
+extend('email', {
+  ...email,
+  message: 'Email must be valid',
+})
 
 export default {
   name: "add-user",
@@ -65,13 +217,55 @@ export default {
         name: "",
         email: "",
         title: "",
-        birthdate: "",
-      },
+        birthdate: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
       submitted: false,
+      select: null,
+      items: [
+        'Item 1',
+        'Item 2',
+        'Item 3',
+        'Item 4',
+      ],
+      checkbox: null,
+      },
+      menu: false,
+      modal: false,
+      menu2: false,
+      nameRules: [
+        v => !!v || 'Name is required',
+        v => (v && v.length >= 1) || 'Name must be at least 1 character',
+      ],
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+      ],
+      titleRules: [
+        v => !!v || 'Title is required',
+        v => (v && v.length >= 1) || 'Title must be at least 1 character',
+      ],
     };
   },
+  components: {
+    ValidationProvider,
+    ValidationObserver,
+  },
   methods: {
-    saveTutorial() {
+    submit () {
+      this.$refs.observer.validate()
+    },
+    clear () {
+      this.name = ''
+      this.phoneNumber = ''
+      this.email = ''
+      this.select = null
+      this.checkbox = null
+      this.$refs.observer.reset()
+    },
+    saveUser() {
+      console.log("saveUser clicked...");
+      console.log("Printing rules for name: ", this.nameRules[0](this.name), this.nameRules[1](this.name));
+      console.log("Printing rules for email: ", this.emailRules[0](this.name), this.emailRules[1](this.name));
+      console.log("Printing rules for title: ", this.titleRules[0](this.name), this.titleRules[1](this.name));
     //   var data = {
     //     title: this.tutorial.title,
     //     description: this.tutorial.description,
